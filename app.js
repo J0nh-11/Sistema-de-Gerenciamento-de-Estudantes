8c5ff6ac449a34dcbcd58142b6945f695b0c1495
J0nh-11 <joaodanielmaster6475@gmail.com>
2026-05-19 09:20:04 -0300

    Resumo: app.js têm somente o necessário; importa o 3 funcionalidades, tem os caminhos principais de rota p/ controlar todo o sistema.

--- app.js
+++ app.js
@@ -1,39 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const connectionFactory = require("./config/db/ConnectionFactory");
-const porta = process.env.PORT || 3000;
+
 const app = express();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
-
-app.post("/login", (req, res) => {
-    res.sendFile(path.join(__dirname, "src", "view", "menu", "telaMenu.html"));
-});
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "src", "view", "menu", "telaMenu.html"));
-});
+                       
+//rotas
+const routerView = require('./src/routes/routerView');
+const pessoaRouter = require('./src/routes/pessoaRouter');
+app.use('/', routerView);
+app.use('/api', pessoaRouter);
 
 app.use((req, res) => {
     res.status(404).send("Página não encontrada");
 });
-//Conexão com o banco:
-(async () => {
-    try {
-        await connectionFactory.connect();
-        app.listen(porta, () => {
-            console.log(`Servidor rodando em http://localhost:${porta}`);
-        });
-    } catch (error) {
-        console.error("Falha ao iniciar o servidor:", error);
-        process.exit(1);
-    }
-})();
-
-process.on("SIGINT", async () => {
-    console.log("Encerrando servidor...");
-    await connectionFactory.end();
-    process.exit(0);
-});
+module.exports = app;
