d372b3e3dd29e8c0732d8d9f3fe3746e28cc17a7
J0nh-11 <joaodanielmaster6475@gmail.com>
2026-05-20 09:01:16 -0300

    Fução start adicionada. O arquivo importa IniBanco.js p/ executar em start e depois iniciar a conexão principal para poder rodar o Sistema de gerenciamento.

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
-const app = require('./app');
+const app = require("./app");
+const InitDatabase = require("./config/db/InitBanco");
 const connectionFactory = require("./config/db/ConnectionFactory");
-
 const porta = process.env.PORT || 3000;
 
-//Conexão com o banco:
-(async () => {
-    try {
-        await connectionFactory.connect();
-        app.listen(porta, () => {
-            console.log(`Servidor rodando em http://localhost:${porta}`);
-        });
-    } catch (error) {
-        console.error("Falha ao iniciar o servidor:", error);
-        process.exit(1);
-    }
-})();
+async function start() {
+    await InitDatabase();
 
-process.on("SIGINT", async () => {
-    console.log("Encerrando servidor...");
-    await connectionFactory.end();
-    process.exit(0);
-});
+    //Conexão com o banco:
+    (async () => {
+        try {
+            await connectionFactory.connect();
+            app.listen(porta, () => {
+                console.log(`Servidor rodando em http://localhost:${porta}`);
+            });
+        } catch (error) {
+            console.error("Falha ao iniciar o servidor:", error);
+            process.exit(1);
+        }
+    })();
+
+    process.on("SIGINT", async () => {
+        console.log("Encerrando servidor...");
+        await connectionFactory.end();
+        process.exit(0);
+    });
+}
+
+start();
