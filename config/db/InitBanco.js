fe160bd886a5cd0fd290c4600edf19fc39b04982
J0nh-11 <joaodanielmaster6475@gmail.com>
2026-05-20 08:55:35 -0300

    Resumo: Arquivo para abrir servidor do sql. Esse arquivo tem uma função que cria o banco automaticamente, depois as tabelas e em seguida executa para encerrar.

---
+++ config/db/InitBanco.js
@@ -0,0 +1,32 @@
+"use strict";
+const fs = require("fs");
+const path = require("path");
+const mysql = require("mysql2/promise");
+
+async function InitDatabase() {
+    const connectTemporaria = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+
+        multipleStatements: true
+    });
+    //Cria o banco se não existir
+    await connectTemporaria.query("CREATE DATABASE IF NOT EXISTS sistema;");
+    console.log("Banco criado/verificado.");
+    //Seleciona Banco
+    await connectTemporaria.query("USE sistema;");
+
+    //Lê o arquivo bd.sql puro
+    const sqlPath = path.join(__dirname, "bd.sql");
+
+    const sql = fs.readFileSync(sqlPath, "utf-8");
+
+    ///Executa slqPath (arquivo com códigos puros sql);
+    await connectTemporaria.query(sql);
+    await connectTemporaria.end();
+    console.log('Conexão temporaria encerrada!')
+    
+}
+
+module.exports = InitDatabase;
