38f1a24a14eeb03bf1f19c4f230d6429becf61b6
J0nh-11 <joaodanielmaster6475@gmail.com>
2026-05-25 07:14:36 -0300

    Esse arquivo é o que cria o usuário adm para que ele aceite as solicitações de cadastros de outros ususuários!

---
+++ config/db/seeders/CreateAdmin.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const bcrypt = require("bcrypt");
+const connectionFactory = require("../ConnectionFactory");
+
+async function adminSeeder() {
+    try {
+        // Verifica se já existe admin
+        const [rows] = await connectionFactory.execute(
+            "SELECT * FROM pessoa WHERE email = ?",
+
+            ["joaodanielmaster6475@gmail.com"],
+        );
+
+        // Se existir, não cria novamente
+        if (rows.length > 0) {
+            console.log("Admin já existe.");
+
+            return;
+        }
+
+        // Criptografa senha
+        const senhaHash = await bcrypt.hash("@jd.dev1", 10);
+
+        // Cria admin
+        await connectionFactory.execute(
+            `INSERT INTO pessoa
+            (matricula, cpf, nome, senha, email, dataNascimento, endereco, cargo)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+
+            [
+                2020228817,
+                "00102200405",
+                "João Daniel Pereira da Silva",
+                senhaHash,
+                "joaodanielmaster6475@gmail.com",
+                "20080719",
+                "Mirante de Atalaia",
+                "admin",
+            ],
+        );
+
+        console.log("Admin criado.");
+    } catch (error) {
+        console.error("Erro no adminSeeder:", error);
+    }
+}
+
+module.exports = adminSeeder;
