740fff27d3b49b7477e34b0fd0c9a09a6928b1d2
J0nh-11 <joaodanielmaster6475@gmail.com>
2026-05-29 17:41:36 -0300

    App.js atualizado, foiretirado linhas desnecessarias e modificado os caminhos das rotas.

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-
 const app = express();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
-                       
+
 //rotas
-const routerView = require('./src/routes/routerView');
-const pessoaRouter = require('./src/routes/pessoaRouter');
-app.use('/', routerView);
-app.use('/api', pessoaRouter);
+
+const routerView = require("./src/routes/routerView");
+const pessoaRouter = require("./src/routes/pessoaRouter");
+const authRouter = require("./src/routes/routerApi");
+const Solicitacoes = require("./src/routes/routerApi")
+
+app.use(routerView);
+app.use(pessoaRouter);
+app.use("/api", authRouter);
+app.use(Solicitacoes);  
 
 app.use((req, res) => {
     res.status(404).send("Página não encontrada");
 });
+
 module.exports = app;
