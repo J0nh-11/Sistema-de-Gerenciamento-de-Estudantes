d3247192527e6746647d43844bdb7cd8b3b328e6
J0nh-11 <joaodanielmaster6475@gmail.com>
2026-06-01 17:20:56 -0300

    Novo DAO, com os 4 métodos.

---
+++ src/repository/ResponsavelDao.js
@@ -0,0 +1,65 @@
+"use strict";
+
+const connectionFactory = require("../../config/db/ConnectionFactory");
+
+class ResponsavelDao {
+    async list() {
+        const [rows] = await connectionFactory.getConnection().execute(`
+           SELECT
+        p.matricula,
+        p.nome,
+        p.email,
+        p.celular,
+        r.parentesco
+        FROM responsavel r
+        INNER JOIN pessoa p
+        ON r.matricula = p.matricula;
+        `);
+
+        return rows;
+    }
+
+    async create(responsavel) {
+        const sql = `
+            INSERT INTO responsavel
+            (matricula, parentesco)
+            VALUES (?, ?)
+        `;
+
+        const values = [responsavel.matricula, responsavel.parentesco];
+
+        const [result] = await connectionFactory
+            .getConnection()
+            .execute(sql, values);
+
+        return result.insertId;
+    }
+
+    async update(responsavel) {
+        const sql = `
+            UPDATE responsavel
+            SET parentesco = ?
+            WHERE matricula = ?
+        `;
+
+        const values = [responsavel.parentesco, responsavel.matricula];
+
+        const [result] = await connectionFactory
+            .getConnection()
+            .execute(sql, values);
+
+        return result.affectedRows;
+    }
+
+    async deletar(matricula) {
+        const [result] = await connectionFactory
+            .getConnection()
+            .execute(`DELETE FROM responsavel WHERE matricula = ?`, [
+                matricula,
+            ]);
+
+        return result.affectedRows;
+    }
+}
+
+module.exports = new ResponsavelDao();
