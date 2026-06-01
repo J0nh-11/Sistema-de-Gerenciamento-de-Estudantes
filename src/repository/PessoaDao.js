aaf1c946494b7a28c30027f35a5908ddfa00bb67
J0nh-11 <joaodanielmaster6475@gmail.com>
2026-06-01 12:53:11 -0300

    Os 4 métodos corrijidos e mudanças no tipo da sixtase.

--- src/repository/PessoaDao.js
+++ src/repository/PessoaDao.js
@@ -1,73 +1,81 @@
 "use strict";
 const connectionFactory = require("../../config/db/ConnectionFactory");
-const PessoaModel = require("../model/PessoaModel");
-const pessoa = new PessoaModel();
 
 class PessoaDao {
     async list() {
-        const [rows] = await connectionFactory
-            .getConnection()
-            .execute("SELECT * FROM pessoa");
-        return rows;
+        const [result] = await connectionFactory.getConnection().execute(`
+                SELECT id, matricula, cpf, nome, email, data_nascimento, endereco, celular, cargo FROM pessoa
+                `);
+        return result;
     }
 
     async create(pessoa) {
+        const sql = `INSERT INTO pessoa (matricula, cpf, nome, senha, email, data_nascimento, endereco, celular,cargo) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+        const values = [
+            pessoa.matricula,
+            pessoa.cpf,
+            pessoa.nome,
+            pessoa.senha,
+            pessoa.email,
+            pessoa.data_nascimento,
+            pessoa.endereco,
+            pessoa.celular,
+            pessoa.cargo,
+        ];
         const [result] = await connectionFactory
             .getConnection()
-            .execute(
-                "INSERT INTO pessoa (matricula, cpf, nome, senha, email, dataNascimento, endereco, cargo) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-                [
-                    pessoa.matricula,
-                    pessoa.cpf,
-                    pessoa.nome,
-                    pessoa.senha,
-                    pessoa.email,
-                    pessoa.dataNascimento,
-                    pessoa.endereco,
-                    pessoa.cargo,
-                ],
-            );
+            .execute(sql, values);
         return result;
     }
 
     async update(pessoa) {
-        const [result] = await connectionFactory.getConnection().execute(
-            `UPDATE pessoa set 
+        const sql = `UPDATE pessoa set 
          cpf = ?,
          nome = ?,
-         senha = ?,
          email = ?,
-         dataNascimento = ?,
+         data_nascimento = ?,
          endereco = ?,
+         celular = ?,
          cargo = ?
          WHERE matricula = ?    
-         `,
-            [
-                pessoa.cpf,
-                pessoa.nome,
-                pessoa.senha,
-                pessoa.email,
-                pessoa.dataNascimento,
-                pessoa.endereco,
-                pessoa.cargo,
-                pessoa.matricula,
-            ],
-        );
-        return result;
-    }
-
-    async delete(matricula) {
+         `;
+        const values = [
+            pessoa.cpf,
+            pessoa.nome,
+            pessoa.email,
+            pessoa.data_nascimento,
+            pessoa.endereco,
+            pessoa.celular,
+            pessoa.cargo,
+            pessoa.matricula,
+        ];
         const [result] = await connectionFactory
             .getConnection()
-            .execute("DELETE from pessoa where matricula = ?", [matricula]);
+            .execute(sql, values);
         return result;
     }
-    async buscarPorEmail(email) {
-        const [rows] = await connectionFactory
-            .getConnection()
-            .execute("SELECT * FROM pessoa WHERE email = ?", [email]);
 
-        return rows[0]; // retorna o primeiro resultado
+    async deletar(id) {
+        const [result] = await connectionFactory
+            .getConnection()
+            .execute("DELETE from pessoa where id = ?", [id]);
+        return result;
+    }
+    async buscarPorMatricula(matricula) {
+        const [result] = await connectionFactory
+            .getConnection()
+            .execute("SELECT * FROM pessoa WHERE matricula = ?", [matricula]);
+
+        return result[0]; // retorna o primeiro resultado
+    }
+    async buscarPorEmail(email) {
+        const sql = `SELECT id, matricula, cpf, nome, senha, email, data_nascimento, endereco, celular,cargo FROM pessoa WHERE email = ?`;
+        const values = [email];
+        const [result] = await connectionFactory
+            .getConnection()
+            .execute(sql, values);
+
+        return result[0]; // retorna o primeiro resultado
     }
 }
 
